5fb02edf90a1643030e9a649da777e3b278ceeb0
Philipp Burgmer <burgmer@w11k.de>
2018-07-16 18:26:49 +0200

    add library

---
+++ projects/lib/src/lib/lib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LibComponent } from './lib.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [LibComponent],
+  exports: [LibComponent]
+})
+export class LibModule { }
