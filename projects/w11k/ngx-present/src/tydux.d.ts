96fcc96adefd531f7f863dcac240479f2cb86f1b
Philipp Burgmer <burgmer@w11k.de>
2018-07-26 14:05:10 +0200

    update dependencies to Angular 6.1

--- projects/w11k/ngx-present/src/tydux.d.ts
+++ projects/w11k/ngx-present/src/tydux.d.ts
@@ -1,3 +1,23 @@
+// referencing the declaration file does not work
+// because of file copy to higher level during angular lib build
+// -> copy the declaration here
+
 declare namespace tydux_types {
   export type ViewTreeState<T> = any;
 }
+
+// above code works, below doesn't
+// and I don't know why
+
+// import {Store} from '@w11k/tydux';
+//
+// declare namespace tydux_types {
+//
+//   export type ViewTreeState<T> = {
+//     [K in keyof T]
+//     : T[K] extends Store<any, infer S> ? S
+//       : T[K] extends object ? ViewTreeState<T[K]>
+//         : never;
+//   };
+//
+// }
