f4766a070388c1c3534a31de3942b6375c379e98
Philipp Burgmer <burgmer@w11k.de>
2018-07-27 14:13:09 +0200

    impr(slide-index-component): use x.y instead of x / y to better visualize nested structure

--- projects/w11k/ngx-present/src/lib/theming/slide-index.component.ts
+++ projects/w11k/ngx-present/src/lib/theming/slide-index.component.ts
@@ -1,27 +1,21 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ActivatedSlide } from '../slide/slide.service';
-import { untilComponentDestroyed } from 'ng2-rx-componentdestroyed';
 import { Coordinates } from '../core/presentation.types';
 
 @Component({
   selector: 'ngx-present-slide-index',
   template: `{{coordinates$ | async}}`
 })
-export class SlideIndexComponent implements OnInit, OnDestroy {
+export class SlideIndexComponent implements OnInit {
   public coordinates$: Observable<string>;
 
   constructor(private readonly slide: ActivatedSlide) { }
 
   ngOnInit() {
     this.coordinates$ = this.slide.coordinates.pipe(
-      map((coordinates: Coordinates) => coordinates.join(' / ')),
-      untilComponentDestroyed(this)
+      map((coordinates: Coordinates) => coordinates.join('.')),
     );
-  }
-
-  ngOnDestroy(): void {
-    // has to be there for componentDestroyed
   }
 }
