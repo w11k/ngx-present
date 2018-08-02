edeb253b66901ea3d00f636e02a120bedbfa2ac6
Philipp Burgmer <burgmer@w11k.de>
2018-08-02 22:42:20 +0200

    cleanup public api of tcc-theme: remove angular components
    they are exposed via the angular module

--- projects/thecodecampus/ngx-present-theme/src/public_api.ts
+++ projects/thecodecampus/ngx-present-theme/src/public_api.ts
@@ -3,6 +3,3 @@
  */
 
 export * from './lib/ngx-present-theme.module';
-export * from './lib/master/regular/tcc-master-regular.component';
-export * from './lib/master/title/tcc-master-title.component';
-export * from './lib/master/section-title/tcc-master-section-title.component';
