cebffc35811f0699867496749e3deb491668da07
Philipp Burgmer <burgmer@w11k.de>
2018-08-02 22:18:57 +0200

    rename selector of title-master from tcc-master-slide-title to tcc-master-title

--- projects/thecodecampus/ngx-present-theme/src/lib/master/title/tcc-master-title.component.ts
+++ projects/thecodecampus/ngx-present-theme/src/lib/master/title/tcc-master-title.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  selector: 'tcc-master-slide-title',
+  selector: 'tcc-master-title',
   templateUrl: './tcc-master-title.component.html',
   styleUrls: ['./tcc-master-title.component.scss']
 })
