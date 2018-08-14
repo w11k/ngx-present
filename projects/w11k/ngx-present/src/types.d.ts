8277247710449c4f5e2949ca37096f73a894c458
Philipp Burgmer <burgmer@w11k.de>
2018-08-14 19:36:54 +0200

    fix tydux's View type definition

--- projects/w11k/ngx-present/src/types.d.ts
+++ projects/w11k/ngx-present/src/types.d.ts
@@ -2,35 +2,29 @@
 // because of file copy to higher level during angular lib build
 // -> copy the declaration here
 
-declare namespace tydux_types {
-  export type ViewTreeState<T> = any;
+import { Store } from '@w11k/tydux';
+
+declare global {
+  namespace tydux_types {
+
+    export type ViewTreeState<T> = {
+      [K in keyof T]
+      : T[K] extends Store<any, infer S> ? S
+        : T[K] extends object ? ViewTreeState<T[K]>
+          : never;
+    };
+
+  }
+
+  interface RecursiveArray<T> extends Array<T | RecursiveArray<T>> {}
+
+  interface ListOfRecursiveArraysOrValues<T> extends Array<T | RecursiveArray<T>> {}
+
+  type RecursivePartial<T> = {
+    [P in keyof T]?:
+    T[P] extends (infer U)[] ? RecursivePartial<U>[] :
+      T[P] extends object ? RecursivePartial<T[P]> :
+        T[P];
+  };
 }
 
-// above code works, below doesn't
-// and I don't know why
-
-// import {Store} from '@w11k/tydux';
-//
-// declare namespace tydux_types {
-//
-//   export type ViewTreeState<T> = {
-//     [K in keyof T]
-//     : T[K] extends Store<any, infer S> ? S
-//       : T[K] extends object ? ViewTreeState<T[K]>
-//         : never;
-//   };
-//
-// }
-
-declare interface RecursiveArray<T> extends Array<T | RecursiveArray<T>> {
-}
-
-declare interface ListOfRecursiveArraysOrValues<T> extends Array<T | RecursiveArray<T>> {
-}
-
-declare type RecursivePartial<T> = {
-  [P in keyof T]?:
-  T[P] extends (infer U)[] ? RecursivePartial<U>[] :
-    T[P] extends object ? RecursivePartial<T[P]> :
-      T[P];
-};
