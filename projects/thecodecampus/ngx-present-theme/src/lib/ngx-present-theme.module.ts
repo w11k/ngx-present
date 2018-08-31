b4eb7460cad984e0b5d1e71a418c87c55d1a4ea3
Philipp Burgmer <burgmer@w11k.de>
2018-08-31 14:25:09 +0200

    add prism's json module

--- projects/thecodecampus/ngx-present-theme/src/lib/ngx-present-theme.module.ts
+++ projects/thecodecampus/ngx-present-theme/src/lib/ngx-present-theme.module.ts
@@ -16,6 +16,7 @@ import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-java';
 import 'prismjs/components/prism-kotlin';
 
