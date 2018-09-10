52e700eac039d0677ed9ebc2753d4550e47b6706
Philipp Burgmer <burgmer@w11k.de>
2018-09-10 15:57:25 +0200

    switch coordinates-separator in HelpService from / to .

--- projects/thecodecampus/ngx-present-theme/src/lib/helper/help/help.component.ts
+++ projects/thecodecampus/ngx-present-theme/src/lib/helper/help/help.component.ts
@@ -30,20 +30,21 @@ export class HelpMutator extends Mutator<HelpState> {
 export class HelpService extends Store<HelpMutator, HelpState> {
 
   private registry = new Map<string, number>();
+  private readonly separator = '.';
 
   constructor() {
     super('MoreDetails', new HelpMutator(), new HelpState());
   }
 
   register(slideCoordinates: Coordinates): Coordinates {
-    const oldCounter = this.registry.get(slideCoordinates.join('/'));
+    const oldCounter = this.registry.get(slideCoordinates.join(this.separator));
 
     let newCounter = 0;
     if (oldCounter !== undefined) {
       newCounter = oldCounter + 1;
     }
 
-    this.registry.set(slideCoordinates.join('/'), newCounter);
+    this.registry.set(slideCoordinates.join(this.separator), newCounter);
 
     return [...slideCoordinates, newCounter];
   }
@@ -51,20 +52,20 @@ export class HelpService extends Store<HelpMutator, HelpState> {
   deregister(detailsCoordinates: Coordinates) {
     const slideCoordinates = detailsCoordinates.slice(0, -1);
 
-    this.registry.delete(slideCoordinates.join('/'));
+    this.registry.delete(slideCoordinates.join(this.separator));
   }
 
   open(slideCoordinates: Coordinates) {
-    this.mutate.setOpen(slideCoordinates.join('/'), true);
+    this.mutate.setOpen(slideCoordinates.join(this.separator), true);
   }
 
   close(slideCoordinates: Coordinates) {
-    this.mutate.setOpen(slideCoordinates.join('/'), false);
+    this.mutate.setOpen(slideCoordinates.join(this.separator), false);
   }
 
   isOpen(detailsCoordinates: Coordinates): ObservableSelection<boolean> {
     return super
-      .select(state => state[detailsCoordinates.join('/')])
+      .select(state => state[detailsCoordinates.join(this.separator)])
       .pipe(map(subState => {
         if (subState !== undefined) {
           return subState.open;
