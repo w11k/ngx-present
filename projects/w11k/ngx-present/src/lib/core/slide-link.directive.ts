0e51990153dc0ed6f456af39bdaecb8789d98af2
Philipp Burgmer <burgmer@w11k.de>
2018-09-16 02:56:01 +0200

    fix slide-link-directive: navigate to presenter via alt-click

--- projects/w11k/ngx-present/src/lib/core/slide-link.directive.ts
+++ projects/w11k/ngx-present/src/lib/core/slide-link.directive.ts
@@ -1,8 +1,15 @@
-import { Directive, Input, OnDestroy, Type } from '@angular/core';
-import { RouterLinkWithHref } from '@angular/router';
+import { Directive, ElementRef, Input, OnDestroy, Optional, Type } from '@angular/core';
+import { Router, RouterLinkWithHref } from '@angular/router';
 import { SlideBySlideService } from '../slide-by-slide/slide-by-slide.service';
 import { toAngularComponent } from '@w11k/tydux/dist/angular-integration';
-import { filter, map, take } from 'rxjs/operators';
+import { filter, map, take, withLatestFrom } from 'rxjs/operators';
+import { ReplaySubject, fromEvent } from 'rxjs';
+import { Slide } from './presentation.types';
+import { untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
+
+function isMouseEvent(event: any): event is MouseEvent {
+  return event instanceof MouseEvent;
+}
 
 /**
  * Use in conjunction with routerLink on an anchor tag
@@ -12,8 +19,47 @@ import { filter, map, take } from 'rxjs/operators';
 })
 export class SlideLinkDirective implements OnDestroy {
 
+  private readonly slide$ = new ReplaySubject<Slide>(1);
+
   constructor(private readonly service: SlideBySlideService,
-              private readonly link: RouterLinkWithHref) {
+              private readonly router: Router,
+              private readonly element: ElementRef,
+              @Optional() private readonly routerLinkDirective: RouterLinkWithHref) {
+
+    if (routerLinkDirective) {
+      this.slide$
+        .pipe(
+          untilComponentDestroyed(this),
+        )
+        .subscribe(slide => {
+          let link: any[];
+
+          if (slide) {
+            link = (['/slide', ...slide.coordinates]);
+          } else {
+            link = [];
+          }
+
+          this.routerLinkDirective.routerLink = link;
+          // update href on link
+          this.routerLinkDirective.ngOnChanges({});
+        });
+    }
+
+    fromEvent(element.nativeElement, 'click')
+      .pipe(
+        filter(isMouseEvent),
+        filter(event => event.altKey),
+        withLatestFrom(this.slide$),
+        untilComponentDestroyed(this),
+      )
+      .subscribe(([event, slide]) => {
+        event.preventDefault();
+
+        // const link = (['/presenter', ...slide.coordinates]);
+        //
+        // this.router.navigate(link);
+      });
   }
 
   @Input()
@@ -25,19 +71,7 @@ export class SlideLinkDirective implements OnDestroy {
         filter(slide => slide !== undefined),
         take(1),
       )
-      .subscribe(slide => {
-        let link: any[];
-
-        if (slide) {
-          link = (['/slide', ...slide.coordinates]);
-        } else {
-          link = [];
-        }
-
-        this.link.routerLink = link;
-        // update href on link
-        this.link.ngOnChanges({});
-      });
+      .subscribe(slide => this.slide$.next(slide));
   }
 
   ngOnDestroy(): void {}
