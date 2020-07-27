fe73c87836b53627cadc530cab1d0671bece1785
Philipp Burgmer <burgmer@pdtec.de>
2020-07-27 17:13:46 +0200

    fix missing keyboard event processors

--- projects/w11k/ngx-present/src/lib/ngx-present-root.module.ts
+++ projects/w11k/ngx-present/src/lib/ngx-present-root.module.ts
@@ -1,9 +1,19 @@
 import { isDevMode, ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TyduxConfiguration, TyduxModule } from '@w11k/tydux-angular';
+import { KEYBOARD_EVENT_PROCESSOR_TOKEN, ToggleSideNav } from './core/event.service';
 import { NGX_PRESENT_CONFIG, SLIDES } from './core/presentation.service';
 import { NgxPresentConfig, SlideComponents } from './core/presentation.types';
 import { NgxPresentModule } from './ngx-present.module';
+import {
+  NavigateSectionBackward,
+  NavigateSectionForward,
+  NavigateSlideBackward,
+  NavigateSlideForward,
+  NavigateToFirstSlide,
+  NavigateToOverview,
+  TogglePresenter
+} from './slide-by-slide/slide-by-slide.service';
 
 export function configFactory(): TyduxConfiguration {
   return {
@@ -35,6 +45,14 @@ export class NgxPresentRootModule {
       ngModule: NgxPresentRootModule,
       providers: [
         { provide: NGX_PRESENT_CONFIG, useValue: config},
+        { provide: KEYBOARD_EVENT_PROCESSOR_TOKEN, useClass: NavigateSectionForward, multi: true},
+        { provide: KEYBOARD_EVENT_PROCESSOR_TOKEN, useClass: NavigateSlideForward, multi: true},
+        { provide: KEYBOARD_EVENT_PROCESSOR_TOKEN, useClass: NavigateSectionBackward, multi: true},
+        { provide: KEYBOARD_EVENT_PROCESSOR_TOKEN, useClass: NavigateSlideBackward, multi: true},
+        { provide: KEYBOARD_EVENT_PROCESSOR_TOKEN, useClass: NavigateToFirstSlide, multi: true},
+        { provide: KEYBOARD_EVENT_PROCESSOR_TOKEN, useClass: ToggleSideNav, multi: true},
+        { provide: KEYBOARD_EVENT_PROCESSOR_TOKEN, useClass: NavigateToOverview, multi: true},
+        { provide: KEYBOARD_EVENT_PROCESSOR_TOKEN, useClass: TogglePresenter, multi: true},
         { provide: SLIDES, useValue: slides}
       ]
     };
